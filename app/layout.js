cfc9219ffb00f0f1c2de26dbff2a0038ebdbf337
petrkucerak <petr@khome.cz>
2024-03-12 15:48:15 +0100

    Link the manifest file

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ export const metadata = {
   title: "OteFEST",
   description:
     "Otevíráme zrekonstruované Salesiánské centrum Pardubice: zázemí pro mladé lidi, prostor pro dobrovolnické a dobročinné (nejen) křesťanské aktivity. Buďte u toho s námi!",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
