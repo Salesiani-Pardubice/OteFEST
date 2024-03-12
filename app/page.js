effede107c822491bb35078e7563b973e08c4f59
petrkucerak <petr@khome.cz>
2024-03-12 10:24:20 +0100

    Create header

--- app/page.js
+++ app/page.js
@@ -1,6 +1,30 @@
+import HeaderLogo from "@/components/header-logo";
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-[#FCE8EA] w-full min-h-[100vh]">
+      <header className="bg-[#DCF0F5] w-full min-h-20 flex flex-wrap items-center justify-center">
+        <HeaderLogo
+          svgPath={`assets/svg/logo_sdb.svg`}
+          name="Logo Salesiánů Dona Bosca"
+          className={`-mx-7`}
+        />
+        <HeaderLogo
+          svgPath={`assets/svg/logo_skm.svg`}
+          name="Logo Salesináského klubu mládeže Pardubice"
+          className={`-mx-7`}
+        />
+        <HeaderLogo
+          svgPath={`assets/svg/logo_cdb_kolecko.svg`}
+          name="Logo centra Don Bosco"
+          className={`-mx-7`}
+        />
+        <HeaderLogo
+          svgPath={`assets/svg/logo_farnost.svg`}
+          name="Logo farnosti Pardubice"
+          className={``}
+        />
+      </header>
       <h1>OteFEST</h1>
     </main>
   );
---
+++ components/header-logo.js
@@ -0,0 +1,10 @@
+export default function HeaderLogo({ svgPath, className, name }) {
+  return (
+    <img
+      src={svgPath}
+      className={`${className} w-36 my-2`}
+      title={name}
+      alt={name}
+    />
+  );
+}
