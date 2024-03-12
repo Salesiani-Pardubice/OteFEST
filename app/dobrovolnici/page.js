b9715ac4c4e12de1f44edf50fee3e936b2c92a4c
petrkucerak <petr@khome.cz>
2024-03-12 14:54:49 +0100

    Create pages with forms

---
+++ app/dobrovolnici/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <iframe
+        width="100%"
+        height="800px"
+        src="https://forms.office.com/Pages/ResponsePage.aspx?id=bhInU04TuUOL6ICP4syiWkLw7VR8dkBEiwwdixwnKtFUQjVUSENXVk5PTDVBNkJWMEhSTVgxMU5PQi4u&embed=true"
+        frameborder="0"
+        marginwidth="0"
+        marginheight="0"
+        className="border-none max-w-full max-h-[100vh]"
+        allowfullscreen
+        webkitallowfullscreen
+        mozallowfullscreen
+        msallowfullscreen
+        id="form"
+      ></iframe>
+    </main>
+  );
+}
